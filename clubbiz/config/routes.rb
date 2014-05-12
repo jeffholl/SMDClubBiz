973e60b074c73f894940cbc22029500b2696ccb2
jeffholl <jeffholl90@gmail.com>
2014-05-12 21:11:38 +1000

    direct rootto clubs and application.html.erb

--- clubbiz/config/routes.rb
+++ clubbiz/config/routes.rb
@@ -3,7 +3,7 @@ Clubbiz::Application.routes.draw do
   resources :clubs
 
   #root route to direct inbound traffic on clean domain named
-  root to: "home#index"
+  root to: "clubs#index"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
