674b06f66513357af65d9a5c7eab2aed06128369
jeffholl <jeffholl90@gmail.com>
2014-05-16 02:40:11 +1000

    fixed shared_event entry deletion when event deleted

--- clubbiz/app/models/event.rb
+++ clubbiz/app/models/event.rb
@@ -2,13 +2,13 @@ class Event < ActiveRecord::Base
 	
 #	belongs_to :club
 
-	has_many :shared_events
+	has_many :shared_events, dependent: :destroy
 	has_many :clubs, :through => :shared_events
 
 	has_many :event_comments
 
 	has_many :tickets
 	
-	accepts_nested_attributes_for :shared_events, allow_destroy: true
+	accepts_nested_attributes_for :shared_events
 
 end
