eb19928af039285b5b30e404a7fea5279479398c
jeffholl <jeffholl90@gmail.com>
2014-05-17 04:25:06 +1000

    add relations from User to Ticket and Club through associated entities

--- clubbiz/app/models/user.rb
+++ clubbiz/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  
+	has_many :ticket_allocations, dependent: :destroy
+	has_many :tickets, :through => :ticket_allocations, dependent: :destroy
+
+	has_many :memberships, dependent: :destroy
+	has_many :clubs, :through => :memberships, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
